cc66867d6ed1a81e30c762a1243f1f86b6868155
Airon <131386167+AironClarke@users.noreply.github.com>
2025-03-22 20:56:10 +0000

    created account toggle component

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,16 @@
+import { AccountToggle } from './AccountToggle'
+import { Search } from './Search'
+
 export const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+      <div className='overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]'>
+        {/* TODO: main sidebar content */}
+        <AccountToggle />
+        <Search />
+      </div>
+
+      {/* TODO: plan toggle */}
+    </div>
   )
 }
