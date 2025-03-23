bde89c12f223770643a02c177cb8801b5d15da4a
Airon <131386167+AironClarke@users.noreply.github.com>
2025-03-23 16:02:38 +0000

    created helper component

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,11 @@
+import Helper from './Helper'
+import Topbar from './Topbar'
+
 export const Dashboard = () => {
   return (
     <div className='bg-white rounded-lg pb-4 shadow h-[200vh]'>
-      Dashboard
+      <Topbar />
+      <Helper />
     </div>
   )
 }
