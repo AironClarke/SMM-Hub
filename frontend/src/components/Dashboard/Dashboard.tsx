45247e80755329b3ed78c8f678be0a8fe2c6d917
Airon <131386167+AironClarke@users.noreply.github.com>
2025-03-29 21:21:42 +0000

    added grid placement

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -4,8 +4,11 @@ import Topbar from './Topbar'
 export const Dashboard = () => {
   return (
     <div className='bg-white rounded-lg pb-4 shadow h-[200vh]'>
-      <Topbar />
-      <Helper />
+       <div className="min-h-screen bg-[linear-gradient(rgba(0,0,0,0.1)_1px,transparent_1px),linear-gradient(90deg,rgba(0,0,0,0.1)_1px,transparent_1px)] bg-[size:55px_55px]
+       h-[2vh]">
+        <Topbar />
+        <Helper />
+        </div>
     </div>
   )
 }
