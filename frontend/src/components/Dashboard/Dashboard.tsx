d6a6a78e31e2220894e1eea36c8efa823d5fe21c
Airon <131386167+AironClarke@users.noreply.github.com>
2025-03-30 17:40:33 +0100

    grid positioning

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -3,12 +3,15 @@ import Topbar from './Topbar'
 
 export const Dashboard = () => {
   return (
-    <div className='bg-white rounded-lg pb-4 shadow h-[200vh]'>
-       <div className="min-h-screen bg-[linear-gradient(rgba(0,0,0,0.1)_1px,transparent_1px),linear-gradient(90deg,rgba(0,0,0,0.1)_1px,transparent_1px)] bg-[size:55px_55px]
-       h-[2vh]">
+    <div className="relative bg-white rounded-lg pb-4 shadow h-[200vh]">
+      {/* Background Grid */}
+      <div className="absolute inset-0 pointer-events-none bg-[linear-gradient(to_right,rgba(0,0,0,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.08)_1px,transparent_1px)] bg-[size:60px_60px] bg-[position:0px_4px]"></div>
+
+      {/* Content */}
+      <div className="relative z-10">
         <Topbar />
         <Helper />
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
