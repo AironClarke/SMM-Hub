46ba76bd41c34e6a6e6ecba286245303fdbfc7d5
Airon <131386167+AironClarke@users.noreply.github.com>
2025-03-30 18:20:02 +0100

    gird now fades at the top

--- frontend/src/components/Dashboard/Test.tsx
+++ frontend/src/components/Dashboard/Test.tsx
@@ -5,7 +5,7 @@ export const Test = () => {
   return (
     <div className="relative bg-white rounded-lg pb-10">
       {/* Background Grid */}
-      <div className="absolute inset-0 pointer-events-none bg-[linear-gradient(to_right,rgba(0,0,0,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.08)_1px,transparent_1px)] bg-[size:55px_55px] bg-[position:0px_1px] h-auto"></div>
+      <div className="absolute inset-0 pointer-events-none bg-[linear-gradient(to_right,rgba(0,0,0,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.08)_1px,transparent_1px)] bg-[size:60px_60px] bg-[position:0px_55px] h-auto"></div>
 
       {/* Content */}
       <div className="relative z-10">
