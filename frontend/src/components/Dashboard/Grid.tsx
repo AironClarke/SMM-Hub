d658ef7a71772d53fd27ec682b784b60dcca6bb6
Airon <131386167+AironClarke@users.noreply.github.com>
2025-03-30 20:40:24 +0100

    Added activity graph

--- frontend/src/components/Dashboard/Grid.tsx
+++ frontend/src/components/Dashboard/Grid.tsx
@@ -1,7 +1,13 @@
+import ActivityGraph from './ActivityGraph';
+import StatCard from './StatCard';
+
 export default function Grid(){
   return (
-    <div>
-      grid
+    <div
+      className="px-4 grid gap-3 grid-cols-12"
+    >
+      <StatCard />
+      <ActivityGraph />
     </div>
   )
 }
