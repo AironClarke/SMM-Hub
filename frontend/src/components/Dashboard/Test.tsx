423a9d5d309be33d7b57929bc2aecbbe28aaf029
Airon <131386167+AironClarke@users.noreply.github.com>
2025-04-01 14:23:01 +0100

    disabled 90char limit due to styling

--- frontend/src/components/Dashboard/Test.tsx
+++ frontend/src/components/Dashboard/Test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import Helper from './Helper'
 import Topbar from './Topbar'
 
@@ -6,7 +7,6 @@ export const Test = () => {
     <div className="relative bg-white rounded-lg pb-14 z-0">
       {/* Background Grid */}
       <div className="absolute inset-0 pointer-events-none bg-[linear-gradient(to_right,rgba(0,0,0,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.08)_1px,transparent_1px)] bg-[size:60px_60px] bg-[position:0px_55px] h-auto"></div>
-
       {/* Content */}
       <div className="relative z-10">
         <Topbar />
