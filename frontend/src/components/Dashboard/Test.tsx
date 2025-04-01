8dd370784ea30266c24f56150732eb229bb2debc
Airon <131386167+AironClarke@users.noreply.github.com>
2025-04-01 13:37:59 +0100

    search menu no longer appears under test container

--- frontend/src/components/Dashboard/Test.tsx
+++ frontend/src/components/Dashboard/Test.tsx
@@ -3,7 +3,7 @@ import Topbar from './Topbar'
 
 export const Test = () => {
   return (
-    <div className="relative bg-white rounded-lg pb-14">
+    <div className="relative bg-white rounded-lg pb-14 z-0">
       {/* Background Grid */}
       <div className="absolute inset-0 pointer-events-none bg-[linear-gradient(to_right,rgba(0,0,0,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.08)_1px,transparent_1px)] bg-[size:60px_60px] bg-[position:0px_55px] h-auto"></div>
 
