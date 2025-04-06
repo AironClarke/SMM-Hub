0fe201bfebb905337d366fcd7fb9eb3f3d9bd50f
Airon <131386167+AironClarke@users.noreply.github.com>
2025-04-06 06:38:47 +0100

    searchbar test

--- frontend/src/utils/SearchBar.tsx
+++ frontend/src/utils/SearchBar.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 
 export default function SearchBar(){
+  const [searchValue, setSearchValue] = useState('')
+
   return (
     <div className="border-1 border-stone-300 mb-4 relative flex
       items-center px-2 py-1.5 text-sm w-56 rounded shadow-md">
       <FiSearch className='mr-2' />
       <input
-        onFocus={(e) => e.target.blur()}
+        onChange={(e) => setSearchValue(e.target.value)}
+        value={searchValue}
         type="text"
         placeholder="Search"
         className="w-full bg-transparent
