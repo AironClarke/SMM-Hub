60b8793acb2d5e51a1fd94b44da159bf733192a0
Airon <131386167+AironClarke@users.noreply.github.com>
2025-04-06 19:56:28 +0100

    Added resources to the nav menu

--- frontend/src/components/Sidebar/RouteSelect.tsx
+++ frontend/src/components/Sidebar/RouteSelect.tsx
@@ -1,14 +1,31 @@
-import { FiDollarSign, FiHome, FiLink, FiPaperclip, FiUsers } from 'react-icons/fi';
+import {
+  FiDatabase,
+  FiDollarSign,
+  FiHome,
+  FiLink,
+  FiPaperclip,
+  FiServer,
+  FiUsers
+} from 'react-icons/fi';
+
 import { Route } from './Route';
 
 export default function RouteSelect() {
   return (
-    <div className="space-y-1">
-      <Route Icon={FiHome} selected={true} title='Dashboard' />
-      <Route Icon={FiUsers} selected={false} title="Team" />
-      <Route Icon={FiPaperclip} selected={false} title="Invoices" />
-      <Route Icon={FiLink} selected={false} title="Integrations" />
-      <Route Icon={FiDollarSign} selected={false} title="Finance" />
-    </div>
+    <>
+      <div className="space-y-1 mb-4">
+        <Route Icon={FiHome} selected={true} title='Dashboard' />
+        <Route Icon={FiUsers} selected={false} title="Team" />
+        <Route Icon={FiPaperclip} selected={false} title="Invoices" />
+        <Route Icon={FiLink} selected={false} title="Integrations" />
+        <Route Icon={FiDollarSign} selected={false} title="Finance" />
+      </div>
+      <div className='mb-4 space-y-1'>
+        <h3 className='px-2 py-1.5'>Resources</h3>
+        <Route Icon={FiUsers} selected={false} title='Users' />
+        <Route Icon={FiServer} selected={false} title="Server" />
+        <Route Icon={FiDatabase} selected={false} title="Database" />
+      </div>
+    </>
   )
 }
